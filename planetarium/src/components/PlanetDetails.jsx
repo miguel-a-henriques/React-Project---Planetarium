011dceac5c0334b2b40147b1a968fb7b5d9af535
Tomás Bastos <soutomasbastos@gmail.com>
2024-02-19 17:50:07 +0000

    updates from monday

--- planetarium/src/components/PlanetDetails.jsx
+++ planetarium/src/components/PlanetDetails.jsx
@@ -1,54 +1,80 @@
 import axios from "axios";
-import { useParams,Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const API_URL = "http://localhost:3000";
 
-function PlanetDetails () {
-    
-    const [planet,setPlanet] = useState({});
+function PlanetDetails() {
+  const [planet, setPlanet] = useState({});
 
-    const {id} = useParams();
+  const { id } = useParams();
 
-    useEffect(()=> {
-        axios.get(`${API_URL}/planets/${id}`)
-        .then((response) => setPlanet(response.data))
-        .catch((error) => console.log(error))
-    },[])
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/planets/${id}`)
+      .then((response) => setPlanet(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    return (
+  return (
+    <div className="details-container">
+      {planet && (
         <div>
-            {planet && <div>
-                <h1>{planet.name}</h1>
-                <section>
-                    {planet.imgSrc && planet.imgSrc.img && <img src={planet.imgSrc.img} alt={planet.imgSrc.imgDescription} />}
-                    {planet.dayLength && <p>Day length: {planet.dayLength}</p>}
-                    {planet.yearLength && <p>Year length: {planet.yearLength}</p>}
-                    {planet.averageTemperature && <p>Average temperature: {planet.averageTemperature}</p>}
-                    {planet.moons && <p>Number of Moons: {planet.moons}</p>}
-                    {planet.basicDetails && planet.basicDetails.volume && <p>Volume: {planet.basicDetails.volume}</p>}
-                    {planet.basicDetails && planet.basicDetails.mass && <p>Mass: {planet.basicDetails.mass}</p>}
-                </section>
-                <section>
-                    {planet.planetOrder && <h2>Order from the Sun: {planet.planetOrder}</h2>}
-                    {planet.description && <h2>{planet.description}</h2>}
-                    <h3>Fun Facts:</h3>
-                    <ul>
-                        {planet.funFacts && planet.funFacts.funFactsOne && <li>{planet.funFacts.funFactsOne}</li>}
-                        {planet.funFacts && planet.funFacts.funFactsTwo &&<li>{planet.funFacts.funFactsTwo}</li>}
-                    </ul>
-                </section>
-                <a href={planet.wikiLink} target="_blank">Wikipedia Link</a>
-                </div>
-                }
-                <Link to={`/planets/edit/${planet.id}`}>
-                {planet && planet.id > 8 && <div>
-                    <button>Know something we don't know?</button>
-                    </div>}
-                </Link>
-                
+          <h1>{planet.name}</h1>
+          <section className="details-container-top">
+            <div className="details-container-img">
+              {planet.imgSrc && planet.imgSrc.img && (
+                <img
+                  src={planet.imgSrc.img}
+                  alt={planet.imgSrc.imgDescription}
+                />
+              )}
+            </div>
+            <div className="details-container-text">
+              {planet.dayLength && <p>Day length: {planet.dayLength}</p>}
+              {planet.yearLength && <p>Year length: {planet.yearLength}</p>}
+              {planet.averageTemperature && (
+                <p>Average temperature: {planet.averageTemperature}</p>
+              )}
+              {planet.moons && <p>Number of Moons: {planet.moons}</p>}
+              {planet.basicDetails && planet.basicDetails.volume && (
+                <p>Volume: {planet.basicDetails.volume}</p>
+              )}
+              {planet.basicDetails && planet.basicDetails.mass && (
+                <p>Mass: {planet.basicDetails.mass}</p>
+              )}
+            </div>
+          </section>
+          <section className="container-description">
+            {planet.planetOrder && (
+              <h3>Order from the Sun: {planet.planetOrder}</h3>
+            )}
+            {planet.description && <p>{planet.description}</p>}
+            <h3>Fun Facts:</h3>
+            <ul>
+              {planet.funFacts && planet.funFacts.funFactsOne && (
+                <li>{planet.funFacts.funFactsOne}</li>
+              )}
+              {planet.funFacts && planet.funFacts.funFactsTwo && (
+                <li>{planet.funFacts.funFactsTwo}</li>
+              )}
+            </ul>
+
+            <a href={planet.wikiLink} target="_blank">
+              Wikipedia Link
+            </a>
+          </section>
         </div>
-    )
+      )}
+      <Link to={`/planets/edit/${planet.id}`}>
+        {planet && planet.id > 8 && (
+          <div>
+            <button>Know something we don't know?</button>
+          </div>
+        )}
+      </Link>
+    </div>
+  );
 }
 
-export default PlanetDetails
+export default PlanetDetails;
--- planetarium/src/pages/AddPlanet.jsx
+++ planetarium/src/pages/AddPlanet.jsx
@@ -1,68 +1,153 @@
-    import axios from "axios";
-    import { useState } from "react";
-    import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-    const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:3000";
 
+function AddPlanet() {
+  const [name, SetName] = useState("");
+  const [description, SetDescription] = useState("");
+  const [wikiLink, setWikiLink] = useState("");
+  const [img, setImg] = useState("");
+  const [moons, setMoons] = useState(0);
+  const [funFactOne, setFunFactOne] = useState("");
+  const [funFactTwo, setFunFactTwo] = useState("");
+  const [dayLength, setDayLength] = useState("");
+  const [yearLength, setYearLength] = useState("");
+  const [averageTemperature, setAverageTemperature] = useState("");
+  const [volume, setVolume] = useState("");
+  const [mass, setMass] = useState("");
 
-    function AddPlanet () {
+  const navigate = useNavigate();
 
-        const [name,SetName] = useState("");
-        const [description,SetDescription] = useState("");
-        const [wikiLink, setWikiLink] = useState("");
-        const [img, setImg] = useState("");
-        const [moons, setMoons] = useState(0);
-        const [funFactOne, setFunFactOne] = useState("");
-        const [funFactTwo, setFunFactTwo] = useState("");
-        const [dayLength, setDayLength] = useState("");
-        const [yearLength, setYearLength] = useState("");
-        const [averageTemperature, setAverageTemperature] = useState("");
-        const [volume, setVolume] = useState("");
-        const [mass, setMass] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        const navigate = useNavigate();
+    const planet = {
+      name,
+      description,
+      wikiLink,
+      imgSrc: { img: img },
+      moons,
+      funFacts: { funFactsOne: funFactOne, funFactsTwo: funFactTwo },
+      dayLength,
+      yearLength,
+      averageTemperature,
+      basicDetails: { mass: mass, volume: volume },
+    };
 
+    axios
+      .post(`${API_URL}/planets`, planet)
+      .then(() => navigate("/"))
+      .catch((error) => console.log(error));
+  }
 
-        function handleSubmit (e) {
+  return (
+    <div className="form-container">
+      <form className="forms" onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          required
+          onChange={(e) => SetName(e.target.value)}
+        />
 
-        e.preventDefault();
+        <label>Description</label>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          required
+          onChange={(e) => SetDescription(e.target.value)}
+        />
+        <label>WikiLink</label>
+        <input
+          type="text"
+          name="wikiLink"
+          value={wikiLink}
+          placeholder="Wikipedia Link"
+          required
+          onChange={(e) => setWikiLink(e.target.value)}
+        />
+        <label>Image</label>
+        <input
+          type="text"
+          name="img"
+          value={img}
+          placeholder="Image URL"
+          required
+          onChange={(e) => setImg(e.target.value)}
+        />
+        <label>Moons</label>
+        <input
+          type="number"
+          name="moons"
+          value={moons}
+          min={0}
+          onChange={(e) => setMoons(e.target.value)}
+        />
+        <label>Fun Fact One</label>
+        <input
+          type="text"
+          name="funFactOne"
+          value={funFactOne}
+          required
+          onChange={(e) => setFunFactOne(e.target.value)}
+        />
+        <label>Fun Fact Two</label>
+        <input
+          type="text"
+          name="funFactTwo"
+          value={funFactTwo}
+          required
+          onChange={(e) => setFunFactTwo(e.target.value)}
+        />
+        <label>Day Length</label>
+        <input
+          type="text"
+          name="dayLength"
+          value={dayLength}
+          placeholder="optional"
+          onChange={(e) => setDayLength(e.target.value)}
+        />
+        <label>Year Length</label>
+        <input
+          type="text"
+          name="yearLength"
+          value={yearLength}
+          placeholder="optional"
+          onChange={(e) => setYearLength(e.target.value)}
+        />
+        <label>Average Temperature</label>
+        <input
+          type="text"
+          name="averageTemperature"
+          value={averageTemperature}
+          placeholder="optional"
+          onChange={(e) => setAverageTemperature(e.target.value)}
+        />
+        <label>Mass</label>
+        <input
+          type="text"
+          name="mass"
+          value={mass}
+          placeholder="optional"
+          onChange={(e) => setMass(e.target.value)}
+        />
+        <label>Volume</label>
+        <input
+          type="text"
+          name="volume"
+          value={volume}
+          placeholder="optional"
+          onChange={(e) => setVolume(e.target.value)}
+        />
+        <button type="submit">Send to Space</button>
+      </form>
+    </div>
+  );
+}
 
-        const planet = {name, description, wikiLink, imgSrc:{img: img}, moons, funFacts:{funFactsOne:funFactOne, funFactsTwo:funFactTwo}, dayLength, yearLength, averageTemperature, basicDetails:{mass: mass, volume: volume}};
-
-        axios.post(`${API_URL}/planets`, planet)
-        .then(()=> navigate('/'))
-        .catch((error) => console.log(error))
-        }
-
-        return (
-            <form onSubmit={handleSubmit}>
-                <label>Name</label>
-                <input type="text" name="name" value={name} required onChange={(e) => SetName(e.target.value)} />
-                <label>Description</label>
-                <input type="text" name="description" value={description} required onChange={(e) => SetDescription(e.target.value)} />
-                <label>WikiLink</label>
-                <input type="text" name="wikiLink" value={wikiLink} placeholder="Wikipedia Link" required onChange={(e) => setWikiLink(e.target.value)} />
-                <label>Image</label>
-                <input type="text" name="img" value={img} placeholder="Image URL"required onChange={(e) => setImg(e.target.value)} />
-                <label>Moons</label>
-                <input type="number" name="moons" value={moons} min={0} onChange={(e) => setMoons(e.target.value)} />
-                <label>Fun Fact One</label>
-                <input type="text" name="funFactOne" value={funFactOne} required onChange={(e) => setFunFactOne(e.target.value)} />
-                <label>Fun Fact Two</label>
-                <input type="text" name="funFactTwo" value={funFactTwo} required onChange={(e) => setFunFactTwo(e.target.value)} />
-                <label>Day Length</label>
-                <input type="text" name="dayLength" value={dayLength} placeholder="optional" onChange={(e) => setDayLength(e.target.value)} />
-                <label>Year Length</label>
-                <input type="text" name="yearLength" value={yearLength} placeholder="optional" onChange={(e) => setYearLength(e.target.value)} />
-                <label>Average Temperature</label>
-                <input type="text" name="averageTemperature" value={averageTemperature} placeholder="optional" onChange={(e) => setAverageTemperature(e.target.value)} />
-                <label>Mass</label>
-                <input type="text" name="mass" value={mass} placeholder="optional" onChange={(e) => setMass(e.target.value)}/>
-                <label>Volume</label>
-                <input type="text" name="volume" value={volume} placeholder="optional" onChange={(e)=> setVolume(e.target.value)}/>
-                <button type="submit">Send to Space</button>
-            </form>
-        )
-    }
-
-    export default AddPlanet
+export default AddPlanet;
