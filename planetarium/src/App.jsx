51af8894c690f4343ccad90f3d3bc04238f5db5b
Tomás Bastos <soutomasbastos@gmail.com>
2024-02-19 10:40:09 +0000

    routes done

--- planetarium/src/App.jsx
+++ planetarium/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <main>
-
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/planet/add" element={<AddPlanet />} />
+        <Route path="/planet/:id" element={<PlanetDetail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
