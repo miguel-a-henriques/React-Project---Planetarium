80feb8ed58e0f16aadc9c7392d4771b4467ab8dc
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-19 14:07:31 +0000

    add planet

--- planetarium/src/pages/AddPlanet.jsx
+++ planetarium/src/pages/AddPlanet.jsx
@@ -1,53 +1,53 @@
-import axios from "axios";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+    import axios from "axios";
+    import { useState } from "react";
+    import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://localhost:3000";
-
-
-function AddPlanet () {
-
-    const [name,SetName] = useState("");
-    const [description,SetDescription] = useState("");
-    const [wikiLink, setWikiLink] = useState("Wikipedia url");
-    const [img, setImg] = useState("Image url");
-    const [moons, setMoons] = useState(0);
-    const [funFactOne, setFunFactOne] = useState("");
-    const [funFactTwo, setFunFactTwo] = useState("");
-
-    const navigate = useNavigate();
+    const API_URL = "http://localhost:3000";
 
 
-    function handleSubmit () {
+    function AddPlanet () {
 
-    e.preventDefault();
+        const [name,SetName] = useState("");
+        const [description,SetDescription] = useState("");
+        const [wikiLink, setWikiLink] = useState("Wikipedia url");
+        const [img, setImg] = useState("Image url");
+        const [moons, setMoons] = useState(0);
+        const [funFactOne, setFunFactOne] = useState("");
+        const [funFactTwo, setFunFactTwo] = useState("");
 
-    const planet = {name, description, wikiLink, imgSrc:{img}, moons, funFacts:{funFactOne, funFactTwo}}
+        const navigate = useNavigate();
 
-    axios.post(`${API_URL}/planets`, planet)
-    .then((response)=> navigate('/'))
-    .catch((error) => console.log)
+
+        function handleSubmit (e) {
+
+        e.preventDefault();
+
+        const planet = {name, description, wikiLink, imgSrc:img, moons, funFacts:{funFactsOne:funFactOne, funFacts:funFactTwo}}
+
+        axios.post(`${API_URL}/planets`, planet)
+        .then(()=> navigate('/'))
+        .catch((error) => console.log(error))
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="name" value={name} required onChange={(e) => SetName(e.target.value)} />
+                <label>Description</label>
+                <input type="text" name="description" value={description} required onChange={(e) => SetDescription(e.target.value)} />
+                <label>WikiLink</label>
+                <input type="text" name="wikiLink" value={wikiLink} required onChange={(e) => setWikiLink(e.target.value)} />
+                <label>Image</label>
+                <input type="text" name="img" value={img} required onChange={(e) => setImg(e.target.value)} />
+                <label>Moons</label>
+                <input type="number" name="moons" value={moons} min={0} onChange={(e) => setMoons(e.target.value)} />
+                <label>Fun Fact One</label>
+                <input type="text" name="funFactOne" value={funFactOne} required onChange={(e) => setFunFactOne(e.target.value)} />
+                <label>Fun Fact Two</label>
+                <input type="text" name="funFactTwo" value={funFactTwo} required onChange={(e) => setFunFactTwo(e.target.value)} />
+                <button type="submit">Send to Space</button>
+            </form>
+        )
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input type="text" name="name" value={name} required onChange={(e) => SetName(e.target.value)} />
-            <label>Description</label>
-            <input type="text" name="description" value={description} required onChange={(e) => SetDescription(e.target.value)} />
-            <label>WikiLink</label>
-            <input type="text" name="wikiLink" value={wikiLink} required onChange={(e) => setWikiLink(e.target.value)} />
-            <label>Image</label>
-            <input type="text" name="img" value={img} required onChange={(e) => setImg(e.target.value)} />
-            <label>Moons</label>
-            <input type="number" name="moons" value={moons} min={0} onChange={(e) => setMoons(e.target.value)} />
-            <label>Fun Fact One</label>
-            <input type="text" name="funFactOne" value={funFactOne} required onChange={(e) => setFunFactOne(e.target.value)} />
-            <label>Fun Fact Two</label>
-            <input type="text" name="funFactTwo" value={funFactTwo} required onChange={(e) => setFunFactTwo(e.target.value)} />
-            <button type="submit">Send to Space</button>
-        </form>
-    )
-}
-
-export default AddPlanet
+    export default AddPlanet
