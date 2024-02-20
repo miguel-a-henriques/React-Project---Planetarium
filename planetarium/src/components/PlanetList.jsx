824ccd29c9dbc391bea4255773e05ca5bef86719
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-20 14:17:09 +0000

    divider made

--- planetarium/src/components/PlanetList.jsx
+++ planetarium/src/components/PlanetList.jsx
@@ -19,6 +19,7 @@ function PlanetList() {
       {planets &&
         planets.map((planet) => {
           return (
+          planet.id <= 8 ? (
             <section key={planet.id} className="cards ">
               <Link to={`/planets/${planet.id}`}>
                 <div className="img-container">
@@ -32,8 +33,39 @@ function PlanetList() {
                 </div>
               </Link>
             </section>
-          );
+          ): 
+          <section className="newPlanets">
+            <div className="divider">
+              <div className="addYourOwn">
+                <h1>Add your Own</h1>
+              </div>
+              <div className="rocketSlider">
+                <img src="https://i.ibb.co/54Cg2QK/96d2ccb9cb708160f82c3d36aba5e0fc-removebg-preview.png" alt="rocket cruising through space"/>
+              </div>
+            </div>
+            <div>
+            <section key={planet.id} className="cards ">
+              <Link to={`/planets/${planet.id}`}>
+                <div className="img-container">
+                  <img
+                    src={planet.imgSrc.img}
+                    alt={planet.imgSrc.imgDescription}
+                  />
+                </div>
+                <div className="name-container">
+                  <h1>{planet.name}</h1>
+                </div>
+              </Link>
+            </section>
+            </div>
+          </section>
+        )
         })}
+        <div className="cards">
+          <Link to={"/planets/add"}>
+            <img src="https://i.ibb.co/LgNwBBq/icons8-add-100.png" alt="add your own planet" />
+          </Link>
+        </div>
     </div>
   );
 }
