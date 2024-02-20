2a41e9cc3d1fc07fc73613035901255b6b6b35ea
Tomás Bastos <soutomasbastos@gmail.com>
2024-02-20 16:16:12 +0000

    more updates

--- planetarium/src/pages/Error.jsx
+++ planetarium/src/pages/Error.jsx
@@ -1,19 +1,19 @@
 function Error() {
-    return (
-      <article>
-        <div>
-          <h1>404</h1>
-        </div>
-        <div className="error">
-          <img
-            src="https://i.ibb.co/P9zBSjS/transparent-astronaut-64a12b3170a2f6-9832789616882839534614.png"
-            alt="floating astronaut"
-          />
-        </div>
-        <div>
-          <h1>Black Hole Found</h1>
-        </div>
-      </article>
-    );
-  }
-  export default Error;
+  return (
+    <article className="errorContainer">
+      <div className="errorCenter">
+        <h1>404</h1>
+      </div>
+      <div className="error">
+        <img
+          src="https://i.ibb.co/P9zBSjS/transparent-astronaut-64a12b3170a2f6-9832789616882839534614.png"
+          alt="floating astronaut"
+        />
+      </div>
+      <div className="errorCenter">
+        <h1>Black Hole Found</h1>
+      </div>
+    </article>
+  );
+}
+export default Error;
