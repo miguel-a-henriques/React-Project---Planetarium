ae2df0c76300de9bafda6ea3aee4d72ac8a54329
Miguel Henriques <miguelaghenriques@gmail.com>
2024-02-21 14:41:57 +0000

    adjusted

--- planetarium/src/App.jsx
+++ planetarium/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   useEffect(()=> {
     const timeOut = setTimeout(()=> {
       setVideo(false);
-    }, 8000);
+    }, 5000);
     return () => clearTimeout (timeOut)
   },[])
   return (
